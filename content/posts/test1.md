14b42a112bbc1a71618c2e3d609653c272934412
Simon Wu <simon345.wu@gmail.com>
2026-07-13 06:44:37 +0800

    vault backup: 2026-07-13 06:44:37

---
+++ content/posts/test1.md
@@ -0,0 +1,7 @@
++++
+title = "Test1"
+date = 2026-07-13
+draft = false
++++
+
+test1
--- public/js/lib/cookieconsent.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  // <stdin>
-  document.addEventListener("DOMContentLoaded", () => {
-    if (window.config.cookieconsent && window.cookieconsent)
-      window.cookieconsent.initialise(window.config.cookieconsent);
-  }, false);
-})();
-//# sourceMappingURL=cookieconsent.js.map
